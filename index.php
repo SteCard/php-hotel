2631cd4e70dc08fd8fd2ff43c079763e1f3bfd8e
SteCard <stefanocardillo14@gmail.com>
2023-07-04 21:21:38 +0200

    struttura tabella (bootstrap) + richiamo array

--- index.php
+++ index.php
@@ -46,13 +46,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Php-hotel</title>
 </head>
 <body>
     <main>
         <div class="container">
             <div class="row">
-
+                <div class="col-12">
+                    <!--  Table -->
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descrizione</th>
+                                <th>Parcheggio</th>
+                                <th>Voto</th>
+                                <th>Distanza dal centro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($hotels as $hotel) {?>
+                                <tr>
+                                    <td>
+                                        <?php echo $hotel['name'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['description'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['parking'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['vote'] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['distance_to_center'] ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </main>
